2c76a2114852e46759b3dc8de4ebdd90d180655c
surajpxl <guptasuraj4455@gmail.com>
2025-12-14 18:58:59 +0530

    setup the backend  base

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+
+const authRouter = require('./Router/authRoute');
+
+// middleware to read JSON body
+app.use(express.json());
+
+// routes
+app.use('/api/auth', authRouter);
+
+// test route
+app.use('/', (req, res) => {
+    res.status(200).json({ data: 'JWT auth server.' });
+});
+
+
+module.exports = app;
